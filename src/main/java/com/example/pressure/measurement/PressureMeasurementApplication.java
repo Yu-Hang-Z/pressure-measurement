ff9f50b7563b93f9e264bdf7f197f085fcf0cca3
ZYHZ <Yu-Hang.Zhang@outlook.com>
2023-12-05 13:34:52 +0800

    1.对项目的慢接口和快接口分别进行压测的项目

---
+++ src/main/java/com/example/pressure/measurement/PressureMeasurementApplication.java
@@ -0,0 +1,13 @@
+package com.example.pressure.measurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressureMeasurementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PressureMeasurementApplication.class, args);
+    }
+
+}
